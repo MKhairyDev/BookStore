fe30bd6d50b6979f23aa00ad2daf88cd14f6b44a
Moemen Saad <moemen.saad@nuance.com>
2021-06-03 18:20:12 +0200

    Add Book & Author entities

---
+++ BookStore.Domain/Entities/Book.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace BookStore.Domain.Entities
+{
+   public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public int PublishDate { get; set; }
+        public List<Author>Authors { get; set; }
+    }
+}
